7ef5d2f3a1526f2e78e31d6a18599aeeeb7e0b29
Benjamin Pineau <ben.pineau@gmail.com>
2017-12-24 19:09:45 +0100

    Upgrade Kubernetes' client-go to v6.0.0 (1.9)
    client-go 6.0.0 is out, with support for Kubernetes 1.9.
    The Sirupsen -> sirupsen hack is obsolete, now that client-go depends
    on a recent docker-distribution version that don't depends on Sirupsen

--- pkg/controllers/controllers_test.go
+++ pkg/controllers/controllers_test.go
@@ -6,10 +6,10 @@ import (
 	"testing"
 	"time"
 
+	"k8s.io/api/core/v1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/watch"
-	"k8s.io/client-go/pkg/api/v1"
 	"k8s.io/client-go/tools/cache"
 
 	"github.com/bpineau/kube-deployments-notifier/config"
